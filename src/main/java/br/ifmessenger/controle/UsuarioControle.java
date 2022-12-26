b1b1307a8bf7e796c2ad990b5142246e3fe292c8
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-12-26 18:12:00 -0300

    implementado ALTERAR/EXCLUIR - CRUD COMPLETO USUARIOS

--- src/main/java/br/ifmessenger/controle/UsuarioControle.java
+++ src/main/java/br/ifmessenger/controle/UsuarioControle.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class UsuarioControle {
@@ -28,6 +31,26 @@ public class UsuarioControle {
     @PostMapping("/salvar")
     public String salvarUsuario(@ModelAttribute("usuario") Usuario usuario) {
         usuarioRepo.save(usuario);
+        return "redirect:/usuarios";
+    }
+    @GetMapping("/usuarios/{id}")
+    public String alterarUsuario(@PathVariable("id") long id, Model model) {
+        Optional<Usuario> usuarioOpt = usuarioRepo.findById(id);
+        if (usuarioOpt.isEmpty()) {
+            throw new IllegalArgumentException("Usuario inválido.");
+        }
+
+        model.addAttribute("usuario", usuarioOpt.get());
+        return "/formulario";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirPessoa(@PathVariable("id") long id) {
+        Optional<Usuario> usuarioOpt = usuarioRepo.findById(id);
+        if (usuarioOpt.isEmpty()) {
+            throw new IllegalArgumentException("Usuario Inválido.");
+        }
+        usuarioRepo.delete(usuarioOpt.get());
         return "redirect:/usuarios";
     }
 }
