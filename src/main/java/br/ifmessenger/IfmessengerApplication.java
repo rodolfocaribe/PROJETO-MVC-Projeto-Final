2caeb785e15dbcf7412337de3dfcfac8b7a08189
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-12-26 14:25:50 -0300

    Initial commit

---
+++ src/main/java/br/ifmessenger/IfmessengerApplication.java
@@ -0,0 +1,13 @@
+package br.ifmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifmessenger/IfmessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfmessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
