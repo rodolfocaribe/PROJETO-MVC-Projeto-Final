62ab91e3e8bbfc886ad1d50b1bd1a7b98bc038fe
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-12-26 15:46:17 -0300

    implementado INCLUIR USUARIOS

--- src/main/java/br/ifmessenger/controle/UsuarioControle.java
+++ src/main/java/br/ifmessenger/controle/UsuarioControle.java
@@ -6,6 +6,8 @@ import br.ifmessenger.usuario.UsuarioRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsuarioControle {
@@ -18,5 +20,14 @@ public class UsuarioControle {
     public String usuarios (Model model) {
         model.addAttribute("listaUsuarios", usuarioRepo.findAll());
         return "usuarios";
+    }
+    @GetMapping("/novo")
+    public String novoUsuario(@ModelAttribute("usuario") Usuario usuario) {
+        return "/formulario";
+    }
+    @PostMapping("/salvar")
+    public String salvarUsuario(@ModelAttribute("usuario") Usuario usuario) {
+        usuarioRepo.save(usuario);
+        return "redirect:/usuarios";
     }
 }
