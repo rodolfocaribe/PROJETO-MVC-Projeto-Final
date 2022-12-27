9683149d2181d1e407a4fc2e948dfb0cf6321dbc
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-12-27 20:30:30 -0300

    Inicio da configuração das validações do Validator

--- src/main/java/br/ifmessenger/modelo/Mensagem.java
+++ src/main/java/br/ifmessenger/modelo/Mensagem.java
@@ -1,5 +1,8 @@
 package br.ifmessenger.modelo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Objects;
@@ -8,8 +11,14 @@ import java.util.Objects;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Mensagem {
+
     private Long id;
+
+    @NotBlank
+    @Size(max = 140, message = "Mensagem deve ter no máximo 140 caracteres")
     private String mensagem;
+
+
     private Usuario remetente, destinatario;
 
 }
--- src/main/java/br/ifmessenger/modelo/Usuario.java
+++ src/main/java/br/ifmessenger/modelo/Usuario.java
@@ -1,6 +1,8 @@
 package br.ifmessenger.modelo;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,18 @@ import java.util.Objects;
 public class Usuario {
 
     private Long id;
+
+    @NotBlank (message = "Digite um nome valido")
     private String nome;
+
+    @NotBlank
+    @Pattern(regexp="^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$", message="Formato do CPF inválido. Digte no formato 000.000.000-00")
     private String cpf;
+
+    @NotBlank (message="Digite um email válido")
+    @Pattern(regexp="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\\\.[a-zA-Z0-9-.]+$", message="Email inválido")
     private String email;
+
+
     private String senha;
 }
